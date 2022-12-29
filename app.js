89de81ce45963d37908e3c1bbd25b8756c4402ec
bennybennardo <professorbennardo@gmail.com>
2022-12-29 16:36:56 -0600

    Populate projects in index.pug

--- app.js
+++ app.js
@@ -5,16 +5,37 @@ const app = express();
 // const router = require('./routes/index');
 // app.use('/', router);
 
-//Middleware setup
+/*MIDDLEWARE SETUP*/
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'))
 
-//Route setup
+/*ROUTE SETUP*/
 const routes = require('./routes');
 app.use (routes);
 
 app.listen(3000, ( () => {
     console.log('This application is running on localhost:3000')
 }))
+
+/*ERROR HANDLERS*/
+
+//404 Handler
+
+// app.use((req, res, next) => {
+
+//     console.log('404 error handler called');
+
+//     res.status(404).render('not-found');
+
+// })
+
+// //Global Handler
+
+//     if (err.status === 404) {
+//         res.status(404).render('not-found', { err });
+//     } else {
+//         err.message = err.message || `Oops! It looks like something went wrong on the server.`
+//         res.status(err.status || 500).render('error', { err })
+//     }
 
 module.exports = app;
